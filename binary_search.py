99a592aa2c8934fa6bc9581c5f00b7896a10761a
Eike Rabetian <tommyrabetian@gmail.com>
2022-08-14 15:04:24 +0200

    Binary search

---
+++ binary_search.py
@@ -0,0 +1,24 @@
+### O(log(n))
+
+
+def binary_search(l, item):
+    if not l:
+        return None
+
+    low = 0
+    high = len(l) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if l[mid] == item:
+            return mid
+        elif l[mid] > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+if __name__ == "__main__":
+    l = [1, 3, 4, 5, 7]
+    item = 5
+    print(binary_search(l, item=item))
