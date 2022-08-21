cf52fd097fb5462c02abd99d9e4cba793292dbfc
Eike Rabetian <tommyrabetian@gmail.com>
2022-08-21 16:10:58 +0200

    Refactor quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,16 +1,22 @@
 #### T: Average: O(n log(n)), Worst: O(n2), S: O(log(n))
 
 
+### This is the simple readable solution, but it's not the most optimized solution
+# and uses more memory as it's not sorting in place.
+# Complexity with constants -> O(3nlog(n)) -> simplified -> O(nlog(n))
+
+
 def quicksort(arr):
     if len(arr) < 2:
         return arr
 
     mid = len(arr) // 2
-    pivot = arr.pop(mid)
-    less = [i for i in arr if i <= pivot]
+    pivot = arr[mid]
+    less = [i for i in arr if i < pivot]
+    equal = [i for i in arr if i == pivot]
     greater = [i for i in arr if i > pivot]
 
-    return quicksort(less) + [pivot] + quicksort(greater)
+    return quicksort(less) + equal + quicksort(greater)
 
 
 if __name__ == "__main__":
